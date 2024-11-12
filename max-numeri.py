44b98b6172f4529431ee38b781ba10854c4bde7e
Pedro Vinicius Alves Diniz <104076903+pedro-vinicius-alves-diniz@users.noreply.github.com>
2024-11-12 14:46:06 +0100

    Update max-numeri.py

--- max-numeri.py
+++ max-numeri.py
@@ -1,5 +1,18 @@
-a = int(input())
-b = int(input())
-c = int(input())
+#Dati tre numeri in input trovare il maggiore
 
-print("O maior número é o:", max(a, b, c))
+# a = int(input("Inserire il valore di a: "))
+# b = int(input("Inserire il valore di b: "))
+# c = int(input("Inserire il valore di c: "))
+
+# a, b, c = map(int, input('Insirire i valori di a, b e c: ').split())
+
+numeri = []
+index = 0
+
+for numeri in range(3):
+    numeri.append(int(input("Insirire un numero ")))
+    
+
+
+print(numeri)
+print("O maior número é o:", max(numeri))
