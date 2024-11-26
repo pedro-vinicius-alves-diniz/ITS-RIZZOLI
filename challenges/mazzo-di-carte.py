452a531466ae4053c68707baaedcb14647ddbebb
Pedro Vinicius Alves Diniz <104076903+pedro-vinicius-alves-diniz@users.noreply.github.com>
2024-11-26 10:03:38 +0100

    Update mazzo-di-carte.py

--- challenges/mazzo-di-carte.py
+++ challenges/mazzo-di-carte.py
@@ -1,7 +1,15 @@
 import random
 
 
-mazzo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+mazzo = []
+for i in range(40):
+    i = random.randint(1, 40)
+
+    while i in mazzo:
+        i = random.randint(1, 40)
+
+    mazzo.append(i)
+
 print(f"Mazzo ordinato: {mazzo}\n")
 
 cuori = mazzo[:10]
